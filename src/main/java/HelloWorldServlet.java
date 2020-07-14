b1c65ee141084d02670a0df5b2e422b749b146fa
Larry Castillo <larrycastillo2009@gmail.com>
2020-07-14 10:44:04 -0500

    hello servlet completion

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+
+        String content = "<h1>Hello World!</h1>";
+
+        PrintWriter out = res.getWriter();
+        out.println(content);
 
     }
 }
